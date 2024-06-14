8bd19402d81b6d80d2daa39c2aaa13b051cf37eb
Aldonist <49703584+Aldonist@users.noreply.github.com>
2024-06-14 14:33:07 -0500

    fix mob carryover and fix collision again

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -16,6 +16,7 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	get_tree().call_group("mobs", "queue_free")
 
 func new_game():
 	score = 0
@@ -23,6 +24,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	
 
 func _on_score_timer_timeout():
 	score += 1
