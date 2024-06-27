d411bb4b31af73f10abb4de66538f5073e18f033
CodeBr1dge <dogemanxd2000@gmail.com>
2024-06-26 22:57:31 -0600

    Completed 2D Game Tutorial
    I have reached up to the Heads up display section of the tutorial. I made some variations for the player character to rotate horizontally left and right when they move, while using the same animation sprite they had for up and down

---
+++ mob.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
+	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
